c05917201c3f2100ac0e1cda502ede5ad2d7b9e6
hieu1 <hieukvhe173339@fpt.edu.vn>
2025-02-19 23:14:20 +0700

    update order

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { getShippers, addShipper, getShipperById } = require("./controllers/manageshipper");
 const { loginShipper } = require("./controllers/Login");
-const { getOrders, changeStatusOrder } = require("./controllers/order");
+const { getOrdersInProgress, changeStatusOrder,getOrderDetails } = require("./controllers/order");
 const { submitContact, getContacts } = require("./controllers/contactController");
 
 const app = express();
@@ -21,7 +21,8 @@ app.post("/api/shippers", addShipper);
 
 app.post("/api/login", loginShipper);
 
-app.get("/api/getOrders",getOrders);
+app.get("/api/getOrdersInProgress",getOrdersInProgress);
+app.get("/api/getOrderDetails/:id",getOrderDetails);
 
 app.get("/api/getShipperById", getShipperById);
 
